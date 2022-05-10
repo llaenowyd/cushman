95bfdb59535c30b89ed573e23d7555af9fdaa81e
Collin Monahan <llaenowyd@gmail.com>
2022-05-09 21:56:15 -0400

    basic sketch of library

---
+++ packages/rito/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src', 'index.ts'),
+      name: '@a110/rito',
+      formats: ['es', 'umd'],
+      fileName: format => `rito.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+})
