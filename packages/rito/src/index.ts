ee12ab47e0d9be9972b94bd8f55bb54235240aff
Collin Monahan <41239783+llaenowyd@users.noreply.github.com>
2022-05-10 20:17:16 -0400

    Vite react-ts lib and app with storybook (#1)

---
+++ packages/rito/src/index.ts
@@ -0,0 +1,1 @@
+export { default as Placard } from './Placard'
---
+++ sites/rito-sb/src/App.tsx
@@ -0,0 +1,11 @@
+import { Placard } from '@a110/rito'
+
+function App() {
+  return (
+    <div className="App">
+      <Placard />
+    </div>
+  )
+}
+
+export default App
