0eeab93e96faabcbd873209d05ed17de6085644f
Collin Monahan <41239783+llaenowyd@users.noreply.github.com>
2022-05-12 19:43:24 -0400

    add Button, css variable theming thru to Storybook (#2)

--- packages/rito/vite.config.ts
+++ packages/rito/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+
+import injectCss from './scripts/build/inject-css'
 
 export default defineConfig({
   plugins: [
     react(),
+    // @ts-ignore
+    injectCss(),
     dts({
       insertTypesEntry: true,
     }),
