2a6784fe79e68b8dfa9bd362ed2aea7a887e2bfc
Collin Monahan <llaenowyd@gmail.com>
2022-05-16 19:39:39 -0400

    better DarkModeToggle story

--- packages/lib/rito/src/index.ts
+++ packages/lib/rito/src/index.ts
@@ -2,5 +2,6 @@ export { default as Button } from './Button'
 export { default as DarkModeToggle } from './DarkModeToggle'
 export { default as Modal } from './Modal'
 export { default as SlideIn } from './SlideIn'
+export * from './constants'
 
 console.warn('__INJECT__')
